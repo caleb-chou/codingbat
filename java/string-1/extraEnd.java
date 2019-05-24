4c99cbc6f68ab297b584d4a0b068647292de7d0f
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-05-24 12:35:50 -0500

    Update extraEnd.java

--- java/string-1/extraEnd.java
+++ java/string-1/extraEnd.java
@@ -1,3 +1,7 @@
+/* Given a string, return a new string made of 3 copies of the last 2 chars of the 
+ * original string. The string length will be at least 2.
+ */
+
 public String extraEnd(String str) {
   String newStr = "";
   for (int i = 0; i < 3; i++) {

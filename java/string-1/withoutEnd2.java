3e84967cfc63acaf3a922e6fa9da38eb9cd72b34
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-06-05 23:56:15 -0500

    Update withoutEnd2.java

--- java/string-1/withoutEnd2.java
+++ java/string-1/withoutEnd2.java
@@ -5,7 +5,7 @@
  */
 
 public String withouEnd2(String str) {
-  if(str.length()>1)
-    return str.substring(1, str.length()-1);
+  if(str.length() > 1)
+    return str.substring(1, str.length() - 1);
   return "";
 }

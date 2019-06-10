7b8209f8f8b7e15d7d95a2f5b3f3d0d79560c58b
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-06-10 00:02:43 -0500

    Update minCat.java

--- java/string-1/minCat.java
+++ java/string-1/minCat.java
@@ -9,9 +9,7 @@
 public String minCat(String a, String b) {
   if(a.length() == b.length())
     return a + b;
-  if(a.length() > b.length()) {
+  if(a.length() > b.length()) 
     return a.substring(a.length() - b.length(), a.length()) + b;
-  } else {
-    return a + b.substring(b.length() - a.length(), b.length());
-  }
+  return a + b.substring(b.length() - a.length(), b.length());
 }

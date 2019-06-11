cbd1eaae15f79e5cca9e493d2a5379af24516fda
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-06-11 01:03:18 -0500

    Update sameStarChar.java

--- java/string-2/sameStarChar.java
+++ java/string-2/sameStarChar.java
@@ -1,11 +1,10 @@
 public boolean sameStarChar(String str) {
-  if(str.isEmpty()||!str.contains("*")||str.matches("[*]+")) return true;
-  for(int i=str.length()-1;i>1;i--) {
-    if(str.charAt(i)=='*'&&i!=str.length()-1) {
-      if(str.charAt(i-1)==str.charAt(i+1))
-      return true;
-      return false;
-    }
+  for(int i = 0; i < str.length(); i++) {
+    if(i != 0 && i != str.length() - 1) {
+      if(str.substring(i, i+1).equals("*")) {
+        if(!str.substring(i-1, i).equals(str.substring(i+1, i+2))) return false;
+      }
+    } 
   }
-  return false;
+  return true;
 }

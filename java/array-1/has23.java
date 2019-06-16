80a1839e5810a16065aacb7dc4f8464b5fe49c38
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-06-16 10:39:21 -0500

    worked on array-1

---
+++ java/array-1/has23.java
@@ -0,0 +1,4 @@
+public boolean has23(int[] nums) {
+  for(int i : nums) if( i == 2 || i == 3 ) return true;
+  return false;
+}
---
+++ java/array-1/middleWay.java
@@ -0,0 +1,4 @@
+public int[] middleWay(int[] a, int[] b) {
+  int[] middle = {a[1],b[1]};
+  return middle;
+}

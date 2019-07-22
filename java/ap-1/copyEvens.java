ca62882521aeb084a9a5dfb3a36a7ff9b651ecca
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-07-22 12:57:58 -0500

    Update copyEvens.java

--- java/ap-1/copyEvens.java
+++ java/ap-1/copyEvens.java
@@ -2,7 +2,7 @@ public int[] copyEvens(int[] nums, int count) {
   int ct = 0;
   int[] output = new int[count];
   
-  for(int n: nums) {
+  for(int n : nums) {
      if(n % 2 == 0 && ct < count) {
       output[ct] = n;
       ct++;

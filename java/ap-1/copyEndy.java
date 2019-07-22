c39a156a065c12be06e245a6c80c0c5a9bb7e930
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-07-22 12:53:44 -0500

    Create copyEndy.java

---
+++ java/ap-1/copyEndy.java
@@ -0,0 +1,16 @@
+public int[] copyEndy(int[] nums, int count) {
+  int[] output = new int[count];
+  int ct = 0;
+  
+  for(int n: nums) {
+    if(isEndy(n) && ct < count) {
+      output[ct] = n;
+      ct++;
+    }
+  }
+  return output;
+}
+
+public boolean isEndy(int n) {
+  return n >= 0 && n <= 10 || n >= 90 && n <= 100;
+}

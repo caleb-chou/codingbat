a5984934811ea6f4bda96d146991602d3811b896
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-07-22 18:25:53 -0500

    Update wordsWithout.java

--- java/ap-1/wordsWithout.java
+++ java/ap-1/wordsWithout.java
@@ -1,8 +1,8 @@
 public String[] wordsWithout(String[] words, String target) {
   int ct = 0, index = 0;
   
-  for(int i = 0; i < words.length; i++) {
-    if(words[i].equals(target)) ct++;
+  for(String str : words) {
+    if(str.equals(target)) ct++;
   }
   
   String[] output = new String[words.length - ct];

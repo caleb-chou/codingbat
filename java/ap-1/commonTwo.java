33690017d0c4a75da6f26487e7fd7f494657fa1b
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-07-23 21:51:00 -0500

    Update commonTwo.java

--- java/ap-1/commonTwo.java
+++ java/ap-1/commonTwo.java
@@ -1,18 +1,21 @@
 public int commonTwo(String[] a, String[] b) {
   int count = 0;
-  String coolcat = Arrays.toString(a).replaceAll("[^a-zA-Z0-9]","");
-  for(String i : b) {
-    int temp = coolcat.length();
-    coolcat = coolcat.replaceAll("[" + i + "]","");
-    if(coolcat.length()!=temp) count++;
-  }
+
+  for(int i = 0; i < a.length; i++) {
+    for(int j = 0; j < b.length; j++) {
+        if(!Arrays.toString(a).substring(0, i * 3).contains(a[i]) && a[i].equals(b[j])) {
+          count++;
+          break;
+        }
+    }
+ }
   return count;
 }
+
 /*
 public int commonTwo(String[] a, String[] b) {
-    HashSet<String> kaleb = new HashSet<String>(Arrays.asList(a));
-    HashSet<String> output = new HashSet<String>();
-    for (String str : b) if (kaleb.contains(str)) output.add(str);
-    return output.size();
+  String coolcat = Arrays.toString(a).replaceAll("[^a-zA-Z0-9]",""), count = "0";
+  for(String i : b) if(coolcat.length() != (coolcat = coolcat.replaceAll("["+i+"]","")).length()) count = "" + (Integer.parseInt(count) + 1);
+  return Integer.parseInt(count);
 }
 */

2b2abe42c5da37f92821929f42a3844cda4ebd86
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-07-23 21:46:55 -0500

    fun

--- java/ap-1/commonTwo.java
+++ java/ap-1/commonTwo.java
@@ -8,3 +8,11 @@ public int commonTwo(String[] a, String[] b) {
   }
   return count;
 }
+/*
+CoolCatChristi wanted to make it shorter so there you go
+public int commonTwo(String[] a, String[] b) {
+  String coolcat = Arrays.toString(a).replaceAll("[^a-zA-Z0-9]",""), count = "0";
+  for(String i : b) if(coolcat.length() != (coolcat = coolcat.replaceAll("["+i+"]","")).length()) count = "" + (Integer.parseInt(count) + 1);
+  return Integer.parseInt(count);
+}
+*/

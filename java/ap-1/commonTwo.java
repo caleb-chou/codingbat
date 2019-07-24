e2158a11a3b5df4bc757891b32159cc531f6c18c
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-07-23 21:12:07 -0500

    Update commonTwo.java

--- java/ap-1/commonTwo.java
+++ java/ap-1/commonTwo.java
@@ -1,13 +1,12 @@
 public int commonTwo(String[] a, String[] b) {
   int count = 0;
-  
+
   for(int i = 0; i < a.length; i++) {
-     if(!Arrays.toString(a).substring(0, i * 3).contains(a[i])) {
-        for(int j = 0; j < b.length; j++) {
-            if(a[i].equals(b[j])) {
-              count++;
-              break;
-            }
+    for(int j = 0; j < b.length; j++) {
+        if(!Arrays.toString(a).substring(0, i * 3).contains(a[i]) 
+        && a[i].equals(b[j])) {
+          count++;
+          break;
         }
     }
  }

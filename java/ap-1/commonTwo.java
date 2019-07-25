5983116b3bb9c4e932cc3131e20d1c925b663cab
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-07-24 20:35:57 -0500

    Revert "fun"
    This reverts commit 2b2abe42c5da37f92821929f42a3844cda4ebd86.
    
    Cigar party

--- java/ap-1/commonTwo.java
+++ java/ap-1/commonTwo.java
@@ -1,6 +1,18 @@
 public int commonTwo(String[] a, String[] b) {
+<<<<<<< HEAD
   String coolcat = Arrays.toString(a).replaceAll("[^a-zA-Z0-9]",""), count = "0";
   for(String i : b) if(coolcat.length() != (coolcat = coolcat.replaceAll("["+i+"]","")).length()) count = "" + (Integer.parseInt(count) + 1);
   return Integer.parseInt(count);
 }
 // Done this way to save lines for a little competition
+=======
+  int count = 0;
+  String coolcat = Arrays.toString(a).replaceAll("[^a-zA-Z0-9]","");
+  for(String i : b) {
+    int temp = coolcat.length();
+    coolcat = coolcat.replaceAll("[" + i + "]","");
+    if(coolcat.length()!=temp) count++;
+  }
+  return count;
+}
+>>>>>>> parent of 2b2abe4... fun
